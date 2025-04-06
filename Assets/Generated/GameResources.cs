a4a0518e4b0be8b4c7d76db1c324fabd24a5b407
MoranSD <93720897+MoranSD@users.noreply.github.com>
2025-04-06 10:58:09 +0300

    ignore gr

--- Assets/Generated/GameResources.cs
+++ Assets/Generated/GameResources.cs
@@ -7,39 +7,6 @@ public static class GameResources
 {
     public static class CMS
     {
-        public static class Abilities
-        {
-            public static CMSEntityPfb Ability_0 => Resources.Load<CMSEntityPfb>("CMS/Abilities/Ability 0");
-            public static CMSEntityPfb Ability_1 => Resources.Load<CMSEntityPfb>("CMS/Abilities/Ability 1");
-            public static CMSEntityPfb Ability_2 => Resources.Load<CMSEntityPfb>("CMS/Abilities/Ability 2");
-            public static CMSEntityPfb Ability_3 => Resources.Load<CMSEntityPfb>("CMS/Abilities/Ability 3");
-            public static CMSEntityPfb Ability_4 => Resources.Load<CMSEntityPfb>("CMS/Abilities/Ability 4");
-            public static CMSEntityPfb Ability_5 => Resources.Load<CMSEntityPfb>("CMS/Abilities/Ability 5");
-            public static CMSEntityPfb Ability_6 => Resources.Load<CMSEntityPfb>("CMS/Abilities/Ability 6");
-        }
-        public static class Events
-        {
-            public static CMSEntityPfb Event_0 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 0");
-            public static CMSEntityPfb Event_1 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 1");
-            public static CMSEntityPfb Event_10 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 10");
-            public static CMSEntityPfb Event_11 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 11");
-            public static CMSEntityPfb Event_12 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 12");
-            public static CMSEntityPfb Event_13 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 13");
-            public static CMSEntityPfb Event_14 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 14");
-            public static CMSEntityPfb Event_15 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 15");
-            public static CMSEntityPfb Event_16 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 16");
-            public static CMSEntityPfb Event_17 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 17");
-            public static CMSEntityPfb Event_18 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 18");
-            public static CMSEntityPfb Event_19 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 19");
-            public static CMSEntityPfb Event_2 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 2");
-            public static CMSEntityPfb Event_3 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 3");
-            public static CMSEntityPfb Event_4 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 4");
-            public static CMSEntityPfb Event_5 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 5");
-            public static CMSEntityPfb Event_6 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 6");
-            public static CMSEntityPfb Event_7 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 7");
-            public static CMSEntityPfb Event_8 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 8");
-            public static CMSEntityPfb Event_9 => Resources.Load<CMSEntityPfb>("CMS/Events/Event 9");
-        }
         public static CMSEntityPfb PlayerModel => Resources.Load<CMSEntityPfb>("CMS/PlayerModel");
     }
 }
